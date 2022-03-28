3c188bbf3bc6a7b5bec304d88af4dbd40cd1eea9
Narrateur <theo.paste@outlook.fr>
2022-03-28 15:45:20 +0200

    mes trucs a moi

--- src/exercice1/Exercice1_0.java
+++ src/exercice1/Exercice1_0.java
@@ -11,6 +11,43 @@ public class Exercice1_0 {
 	public Exercice1_0() {
 		space.addElement(robi);
 		space.open();
+		int posX = robi.getX();
+		int posY = robi.getY();
+
+		
+		boolean retourX = false;
+		boolean retourY = false;
+		while(true){
+			if(posX < space.getWidth()-robi.getWidth() && !retourX && !retourY){
+				retourY = false;
+				robi.setX(posX+1);
+				posX = robi.getX();
+			}else{
+				retourX = true;
+				if(posY < space.getHeight()-robi.getHeight() && !retourY && retourX){
+					robi.setY(posY+1);
+					posY = robi.getY();
+				}else{
+					retourY = true;
+					if(posX>0 && retourX){
+						robi.setX(posX-1);
+						posX = robi.getX();
+					}else if(posY > 0 && retourY){
+						retourX = false;
+						robi.setY(posY-1);
+						posY = robi.getY();
+					}else{
+						retourY = false;
+					}
+				}
+			}
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
