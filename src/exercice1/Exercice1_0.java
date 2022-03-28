b4a9dd780466cf94f9d07500346df711f2e77091
MalCaor <xav.lemen@orange.fr>
2022-03-28 15:42:26 +0200

    ex 1

--- src/exercice1/Exercice1_0.java
+++ src/exercice1/Exercice1_0.java
@@ -1,6 +1,11 @@
 package exercice1;
 
+import java.awt.Color;
 import java.awt.Dimension;
+
+import java.util.Random;
+
+import graphicLayer.GElement;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 
@@ -8,9 +13,49 @@ public class Exercice1_0 {
 	GSpace space = new GSpace("Exercice 1", new Dimension(200, 150));
 	GRect robi = new GRect();
 
+	Random rand = new Random();
+
 	public Exercice1_0() {
 		space.addElement(robi);
 		space.open();
+		boolean retour = false;
+		while (true) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if(!retour)
+			{
+				if(robi.getX() < (space.getWidth()-20)){
+					robi.setX(robi.getX()+5);
+					continue;
+				}
+				if(robi.getY() < (space.getHeight()-20)){
+					robi.setY(robi.getY()+5);
+					continue;
+				}
+				retour = true;
+			} else {
+				if(robi.getX() > 0){
+					robi.setX(robi.getX()-5);
+					continue;
+				}
+				if(robi.getY() > 0){
+					robi.setY(robi.getY()-5);
+					continue;
+				}
+				retour = false;
+				// Java 'Color' class takes 3 floats, from 0 to 1.
+				float r = rand.nextFloat();
+				float g = rand.nextFloat();
+				float b = rand.nextFloat();
+				Color randomColor = new Color(r, g, b);
+				robi.setColor(randomColor);
+			}
+
+		}
 	}
 
 	public static void main(String[] args) {
