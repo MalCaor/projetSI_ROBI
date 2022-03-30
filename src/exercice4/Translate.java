13ae35ca5d17c3ca611970ad5647a3e5967e724e
MalCaor <xav.lemen@orange.fr>
2022-03-30 16:31:46 +0200

    translate

--- src/exercice4/Translate.java
+++ src/exercice4/Translate.java
@@ -1,5 +1,7 @@
 package exercice4;
 
+import java.awt.Point;
+
 import graphicLayer.*;
 import stree.parser.SNode;
 
@@ -7,8 +9,7 @@ public class Translate implements Command{
     public Translate(){}
 
     public Reference run(Reference ref, SNode methode){
-        
-        
+        ((GElement)ref.receiver).translate(new Point(Integer.parseInt(methode.get(2).contents()), Integer.parseInt(methode.get(3).contents())));
         return ref;
     }
 }
