537fda4151b17b0d4a3dab19add28308a4c78338
MalCaor <xav.lemen@orange.fr>
2022-03-30 16:27:27 +0200

    set dim

--- src/exercice4/SetDim.java
+++ src/exercice4/SetDim.java
@@ -1,6 +1,5 @@
 package exercice4;
 
-import graphicLayer.GElement;
 import graphicLayer.GSpace;
 import stree.parser.SNode;
 
@@ -8,6 +7,12 @@ public class SetDim implements Command{
     public SetDim(){}
 
     public Reference run(Reference ref, SNode methode){
+        try {
+            ((GSpace)ref.receiver).setSize(Integer.parseInt(methode.get(2).contents()), Integer.parseInt(methode.get(3).contents()));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
         return ref;
     }
 }
