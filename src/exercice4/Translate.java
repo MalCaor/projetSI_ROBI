a1b663fee46d6cc8218471ab6b34bb52b3628674
MalCaor <xav.lemen@orange.fr>
2022-03-30 16:19:22 +0200

    add

---
+++ src/exercice4/Translate.java
@@ -0,0 +1,13 @@
+package exercice4;
+
+import graphicLayer.GElement;
+import graphicLayer.GSpace;
+import stree.parser.SNode;
+
+public class Translate {
+    public Translate(){}
+
+    public Reference run(Reference ref, SNode methode){
+        return ref;
+    }
+}
