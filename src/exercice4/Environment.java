9375eb44e2ed29320926358af6626e3aa6a2b411
MalCaor <xav.lemen@orange.fr>
2022-03-30 13:49:27 +0200

    hashmap env

--- src/exercice4/Environment.java
+++ src/exercice4/Environment.java
@@ -1,10 +1,10 @@
 package exercice4;
 
-import java.util.Dictionary;
+import java.util.HashMap;
 
 public class Environment {
 
-    public Dictionary<String, Reference> listRef;
+    public HashMap<String, Reference> listRef;
 
     public void addReference(String string, Reference spaceRef) {
         listRef.put(string, spaceRef);
