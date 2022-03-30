eb18c062ebd77fd5983c0d650156aa892b7b624c
Narrateur <theo.paste@outlook.fr>
2022-03-30 13:59:06 +0200

    mhè

--- src/exercice4/Reference.java
+++ src/exercice4/Reference.java
@@ -1,15 +1,28 @@
 package exercice4;
 
-public class Reference {
-    Command getCommandByName(String selector){
+import java.util.HashMap;
+import java.util.Map;
+import stree.parser.SNode;
 
+public class Reference {
+
+    Object receiver;
+    Map<String, Command> primitives;
+    
+    public Reference(Object receiver) {
+        this.receiver = receiver;
+        primitives = new HashMap<String, Command>();
+    }
+
+    Command getCommandByName(String selector){
+        return primitives.get(selector);
     }
 
     void addCommand(String selector, Command primitive){
-
+        primitives.put(selector, primitive);
     }
 
-    Referfence run(SNode method){
-        
+    Reference run(SNode method){
+        return null;
     }
 }
