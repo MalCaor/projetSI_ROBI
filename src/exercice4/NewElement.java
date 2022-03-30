ba68fed20409f8a8516b452e02e109726cc84321
MalCaor <xav.lemen@orange.fr>
2022-03-30 16:15:56 +0200

    new element

--- src/exercice4/NewElement.java
+++ src/exercice4/NewElement.java
@@ -1,26 +1,25 @@
 package exercice4;
 
 import stree.parser.SNode;
+
+import java.lang.reflect.InvocationTargetException;
+
 import graphicLayer.*;
 
-public class NewElement implements Command{
-    public NewElement(){}
-
-    public void run(Reference ref, SNode methode){
-        String objet = methode.get(3).contents();
-        
-        switch(objet){
-            case "Rect":
-            
-                break;
-            case "Oval":
-            
-                break;
-            case "Image":
-                
-                break;
-            case "Label":
-                break;
-        }
-    }
+class NewElement implements Command {
+	public Reference run(Reference reference, SNode method) {
+		try {
+			@SuppressWarnings("unchecked")
+			GElement e = ((Class<GElement>) reference.getReceiver()).getDeclaredConstructor().newInstance();
+			Reference ref = new Reference(e);
+			ref.addCommand("setColor", new SetColor());
+			ref.addCommand("translate", new Translate());
+			ref.addCommand("setDim", new SetDim());
+			return ref;
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
