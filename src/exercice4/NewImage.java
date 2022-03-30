92cf7f11c5200bc6d8ac65efb0fed23196731164
MalCaor <xav.lemen@orange.fr>
2022-03-30 15:13:11 +0200

    imagfe

--- src/exercice4/NewImage.java
+++ src/exercice4/NewImage.java
@@ -1,5 +1,25 @@
 package exercice4;
 
-public class NewImage {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import graphicLayer.*;
+import stree.parser.SNode;
+
+public class NewImage implements Command{
+    public NewImage(){}
+
+    public void run(Reference ref, SNode methode){
+        BufferedImage image;
+        try {
+            image = ImageIO.read(new File(methode.get(2).contents()));
+            ((GSpace)ref.receiver).addElement(new GImage(image));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
