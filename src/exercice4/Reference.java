6362b9a899a71641a67c2ad13c9bb7e95dd44dd7
Narrateur <theo.paste@outlook.fr>
2022-03-30 13:42:46 +0200

    debut references

--- src/exercice4/Reference.java
+++ src/exercice4/Reference.java
@@ -1,5 +1,15 @@
 package exercice4;
 
 public class Reference {
+    Command getCommandByName(String selector){
 
+    }
+
+    void addCommand(String selector, Command primitive){
+
+    }
+
+    Referfence run(SNode method){
+        
+    }
 }
