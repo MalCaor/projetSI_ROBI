b2019790e470b180b81096dabf570f7eea3734b1
Narrateur <theo.paste@outlook.fr>
2022-03-30 16:28:26 +0200

    translate

--- src/exercice4/Translate.java
+++ src/exercice4/Translate.java
@@ -1,13 +1,14 @@
 package exercice4;
 
-import graphicLayer.GElement;
-import graphicLayer.GSpace;
+import graphicLayer.*;
 import stree.parser.SNode;
 
-public class Translate {
+public class Translate implements Command{
     public Translate(){}
 
     public Reference run(Reference ref, SNode methode){
+        
+        
         return ref;
     }
 }
