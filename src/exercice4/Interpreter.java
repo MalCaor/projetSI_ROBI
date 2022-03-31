7725f823f9baed01c75698dad53c6e63467482d5
MalCaor <xav.lemen@orange.fr>
2022-03-31 12:55:46 +0200

    interp

--- src/exercice4/Interpreter.java
+++ src/exercice4/Interpreter.java
@@ -5,22 +5,15 @@ import stree.parser.SNode;
 public class Interpreter {
 
     public void compute(Environment environment, SNode next) {
-        System.out.println(next.size());
-        int i=0;
-        while(next.hasChildren()) {
-            if(next.get(i).hasChildren()) {
-                /*int j=0;
-                while(next.get(i).hasChildren()) {
-                    System.out.println(next.get(i).get(j).contents());
-                    j++;
-                }*/
-                compute(environment,next.get(i));
-            }else{
-                System.out.println(next.get(i).contents());
-            }
-            i++;
+        int size = next.size();
+        if(size == 3){
+            // une seul instruction
+            environment.getReferenceByName(next.get(0).contents()).run(next);
+        } else {
+            // itération
+            compute(environment, next.get(3));
+            environment.getReferenceByName(next.get(0).contents()).run(next);
         }
-        //environment.getReferenceByName(next.get(0).contents()).run(next);;
     }
 
 }
