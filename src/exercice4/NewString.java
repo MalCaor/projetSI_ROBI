e3e28250fbbf6db9d74b91f610f49cbab197a712
MalCaor <xav.lemen@orange.fr>
2022-03-31 16:41:41 +0200

    color string

--- src/exercice4/NewString.java
+++ src/exercice4/NewString.java
@@ -11,6 +11,7 @@ public class NewString implements Command{
         ref.receiver = new GString(methode.get(2).contents());
         ref.addCommand("translate", new Translate());
         ref.addCommand("setDim", new SetDim());
+        ref.addCommand("setColor", new SetColor());
         Environment.addReference(methode.get(methode.size()-1).contents(),ref);
         return ref;
     }
