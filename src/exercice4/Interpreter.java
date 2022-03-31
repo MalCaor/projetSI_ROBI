530d732a028fff876638284ad9e41afbd1bc346b
Narrateur <theo.paste@outlook.fr>
2022-03-31 11:54:44 +0200

    test pour interpreter

--- src/exercice4/Interpreter.java
+++ src/exercice4/Interpreter.java
@@ -5,7 +5,22 @@ import stree.parser.SNode;
 public class Interpreter {
 
     public void compute(Environment environment, SNode next) {
-        environment.getReferenceByName(next.get(0).contents()).run(next);;
+        System.out.println(next.size());
+        int i=0;
+        while(next.hasChildren()) {
+            if(next.get(i).hasChildren()) {
+                /*int j=0;
+                while(next.get(i).hasChildren()) {
+                    System.out.println(next.get(i).get(j).contents());
+                    j++;
+                }*/
+                compute(environment,next.get(i));
+            }else{
+                System.out.println(next.get(i).contents());
+            }
+            i++;
+        }
+        //environment.getReferenceByName(next.get(0).contents()).run(next);;
     }
 
 }
