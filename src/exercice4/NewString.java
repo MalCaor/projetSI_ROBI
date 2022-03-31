5ba8b431391acf0428841874a6c968c13e47a6f1
MalCaor <xav.lemen@orange.fr>
2022-03-31 16:40:12 +0200

    string

--- src/exercice4/NewString.java
+++ src/exercice4/NewString.java
@@ -8,7 +8,10 @@ public class NewString implements Command{
     public NewString(){}
 
     public Reference run(Reference ref, SNode methode){
-        ((GSpace)ref.receiver).addElement(new GString(methode.get(2).contents()));
+        ref.receiver = new GString(methode.get(2).contents());
+        ref.addCommand("translate", new Translate());
+        ref.addCommand("setDim", new SetDim());
+        Environment.addReference(methode.get(methode.size()-1).contents(),ref);
         return ref;
     }
 }
