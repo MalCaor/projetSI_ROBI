6b45d05f58a38f4e7104573b19a4a36b36616eb8
Narrateur <theo.paste@outlook.fr>
2022-03-31 14:07:12 +0200

    tout ok normalement

--- src/exercice4/NewElement.java
+++ src/exercice4/NewElement.java
@@ -19,19 +19,30 @@ class NewElement implements Command {
 			switch (method.get(0).contents()){
 				case "rect.class":
 					Reference refRect = new Reference(new GRect());
+
 					refRect.addCommand("setColor", new SetColor());
+					refRect.addCommand("translate", new Translate());
+					refRect.addCommand("setDim", new SetDim());
+
 					Environment.addReference(method.get(method.size()-1).contents(), refRect);
 					return refRect;
+
 				case "oval.class":
 					Reference refOval = new Reference(new GOval());
+
 					refOval.addCommand("setColor", new SetColor());
+					refOval.addCommand("translate", new Translate());
+					refOval.addCommand("setDim", new SetDim());
+
 					Environment.addReference(method.get(method.size()-1).contents(), refOval);
 					return refOval;
+
 				case "image.class":
 					Reference refImage = new NewImage().run(reference, method);
 					refImage.addCommand("setColor", new SetColor());
 					Environment.addReference(method.get(method.size()-1).contents(),refImage);
 					return refImage;
+
 				case "label.class":
 					Reference refLabel = new NewString().run(reference, method);
 					refLabel.addCommand("setColor", new SetColor());
